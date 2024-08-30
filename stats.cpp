af6ea292bd75e04b6367cd7ff32aea384e0a5274
Anila S <104912949+Anila-S@users.noreply.github.com>
2024-08-31 00:17:50 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,21 @@
 #include "stats.h"
-
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+#include <numeric>
+#include <algorithm>
+#include <cmath>
+Stats Statistics::ComputeStatistics(const std::vector<float>& ip) {
     //Implement statistics here
+    stats st;
+    if(ip.size()==0)
+    {
+        st.average=NAN;
+        st.min=
+        st.max=
+    }
+    else
+    {
+        st.average= ((float)std::accumulate(ip.begin(),ip.end(),0.0f))/ip.size();
+        st.min =*std::min_element(ip.begin(), ip.end());
+        st.max =*std::max_element(ip.begin(),ip.end());
+    }
+    return st; 
 }
